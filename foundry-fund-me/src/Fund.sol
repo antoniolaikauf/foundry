234a5aaf0b5b56b0e36d7e25f5553281f08bbf2a
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-10-13 11:12:34 +0200

    compile smart contract

--- foundry-fund-me/src/Fund.sol
+++ foundry-fund-me/src/Fund.sol
@@ -7,6 +7,7 @@ pragma solidity ^0.8.28;
 // import {AggregatorV3Interface} from "./../lib/chainlink-brownie-contracts/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 // secondo modo con remapping in foundry.toml
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./PriceConverter.sol";
 
 error FundMe__NotOwner();
 
@@ -22,8 +23,8 @@ contract fundme {
         _;
     }
 
-    constructor(address priceFeed) {
-        s_priceFeed = AggregatorV3Interface(priceFeed);
+    // s_priceFeed = AggregatorV3Interface(priceFeed);
+    constructor() {
         i_owners = msg.sender;
     }
 }
