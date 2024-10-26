cbfcb567396c03d78eb87e806220de3b66741545
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-10-26 10:54:17 +0200

    lotteria_contratto

--- lotteria/src/Lotteria.sol
+++ lotteria/src/Lotteria.sol
@@ -2,15 +2,20 @@
 pragma solidity ^0.8.19;
 
 contract Lotteria {
+    error NotEnoughEth();
     uint256 constant MINIMUN_ENTRANCE = 0.1 ether;
-    uint256[] partecipanti;
+    uint256 vincita;
+    address payable[] private partecipanti; // gli address sono payable cosi che possono ricevere soldi della vincita
 
-    function enterRuffle(uint256 _enter_pool) public payable {
-        require(_enter_pool * 1e18 >= MINIMUN_ENTRANCE);
-        partecipanti.push(_enter_pool);
+    function enterRuffle(uint256 _price_person) public payable {
+        // rinvertire se manda meno ether
+        // require(_price_person * 1e18 >= MINIMUN_ENTRANCE, NotEnoughEth()); // funziona con solo una versione di compilatore
+        if (_price_person * 1e18 < MINIMUN_ENTRANCE) revert NotEnoughEth();
+        vincita += _price_person;
+        partecipanti.push(payable(msg.sender));
     }
 
-    function winner() public view returns (uint256) {
-        return partecipanti[0];
+    function winner() public view returns (address, uint256) {
+        return (partecipanti[0], vincita);
     }
 }
