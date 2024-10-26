e8352febfcaa1502e672609efcb2bcdc5535afac
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-10-26 11:23:30 +0200

    lotteria_contratto

--- lotteria/src/Lotteria.sol
+++ lotteria/src/Lotteria.sol
@@ -6,11 +6,12 @@ contract Lotteria {
     uint256 constant MINIMUN_ENTRANCE = 0.1 ether;
     uint256 vincita;
     address payable[] private partecipanti; // gli address sono payable cosi che possono ricevere soldi della vincita
-
+    event addressStore(address);
     function enterRuffle(uint256 _price_person) public payable {
         // rinvertire se manda meno ether
         // require(_price_person * 1e18 >= MINIMUN_ENTRANCE, NotEnoughEth()); // funziona con solo una versione di compilatore
         if (_price_person * 1e18 < MINIMUN_ENTRANCE) revert NotEnoughEth();
+        emit addressStore(msg.sender);
         vincita += _price_person;
         partecipanti.push(payable(msg.sender));
     }
