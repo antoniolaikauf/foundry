4f848db79e54a4360d6889df13a29f50fe54a937
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-11-07 10:27:17 +0100

    random number

--- lotteria/src/Lotteria.sol
+++ lotteria/src/Lotteria.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19;
 
-import {VRFCoordinatorV2Interface} from "../lib/chainlink-brownie-contracts/contracts/src/v0.8/vrf/interfaces/VRFCoordinatorV2Interface.sol";
+import {VRFConsumerBaseV2Plus} from "../lib/chainlink-brownie-contracts/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from '../lib/chainlink-brownie-contracts/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol';
 
-contract Lotteria is VRFCoordinatorV2Interface  {
+contract Lotteria is VRFConsumerBaseV2Plus {
     error NotEnoughEth();
     uint256 private immutable i_MINIMUN_ENTRANCE;
     // ogni quanto si estrae un vincitore
@@ -14,11 +15,18 @@ contract Lotteria is VRFCoordinatorV2Interface  {
     uint256 vincita;
     address payable[] private partecipanti; // gli address sono payable cosi che possono ricevere soldi della vincita
     event addressStore(address);
-
-    constructor(uint256 _entra_fee, uint256 _time) {
+    //  P.S  quando si eredita un contratto che ha un contructor allora bisogna metterlo nel costructor del contratto da cui eredita
+    constructor(
+        uint256 _entra_fee,
+        uint256 _time,
+        address _VRcordinator
+    ) VRFConsumerBaseV2Plus(_VRcordinator) {
         i_MINIMUN_ENTRANCE = _entra_fee;
         i_time_winner = _time;
         i_last_time = block.timestamp;
+        // questa variabile non viene dichiarata in questo contratto perchè in realà viene ereditata e si trova nel contratto VRFConsumerBaseV2Plus
+
+        s_vrfCoordinator.requestRandomWords();
     }
     function enterRuffle(uint256 _price_person) public payable {
         // rinvertire se manda meno ether
@@ -31,13 +39,22 @@ contract Lotteria is VRFCoordinatorV2Interface  {
 
     function winner() public returns (address, uint256) {
         if (block.timestamp - i_last_time < i_time_winner) revert();
+// request sarebbe uno struct vedere contract VRFV2PlusClient
+        VRFV2PlusClient.RandomWordsRequest request = VRFV2PlusClient
+            .RandomWordsRequest({
+                keyHash: s_keyHash,
+                subId: s_subscriptionId,
+                requestConfirmations: requestConfirmations,
+                callbackGasLimit: callbackGasLimit,
+                numWords: numWords,
+                extraArgs: VRFV2PlusClient._argsToBytes(
+                    VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+                )
+            });
 
-        requestId = COORDINATOR.requestRandomWords(
-            s_keyHash,
-            s_subscriptionId,
-            requestConfirmations,
-            callbackGasLimit,
-            numWords
+
+        uint256 requestId = s_vrfCoordinator.requestRandomWords(
+           request
         );
 
         i_last_time = block.timestamp;
