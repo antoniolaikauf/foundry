6fa9c0f8eee4a8587062ef951f1a991f6708e816
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-11-12 16:53:45 +0100

    type

--- ES/solidity2.sol
+++ ES/solidity2.sol
@@ -1,10 +1,15 @@
-
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
 
-//type C is V;, where C is the name of the newly introduced type and V has to be a built-in value type 
+//type C is V;, where C is the name of the newly introduced type and V has to be a built-in value type
 type Price is uint128;
-// queste tipo di variabili scelte dall'user possono avere due funzioni 
-Price.wrap()  che prende il valore (che è un uint128) all'interno e lo fa diventare di tipo Price
-Price.unwrap() che prende un valore di tipo Price e lo ritrasforma di tipo uint128 
-di solito si utilizza questo per migliorare la leggibilità e la sicurezza 
+
+contract Math {
+    function add(Price _a1, Price _a2) public pure returns (Price) {
+        Price c = Price.wrap(Price.unwrap(_a1) + Price.unwrap(_a2));
+        // assert non pupò essere applicato a type Price
+        // evita overflow e underflow
+        assert(Price.unwrap(c) >= Price.unwrap(_a1));
+        return c;
+    }
+}
